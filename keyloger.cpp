bf13da8f17c96f5e466398499f5be6f4b4e8bc7b
CollinsMunene <46327225+CollinsMunene@users.noreply.github.com>
2019-07-17 11:26:34 +0300

    Uploaded

---
+++ keyloger.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+
+using namespace std;
+
+
+int main(){
+    char c = 'a', c1 = 67;
+    cout << c << " :: " << c1;
+    
+    return 0;
+}
